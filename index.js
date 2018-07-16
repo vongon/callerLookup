727773f9376eecc5eb2dafe5f371b4b4a952fee0
Ryan James McGill <ryanjamesmcgill@gmail.com>
2018-07-16 10:46:26 -0500

    look up all phone numbers

--- index.js
+++ index.js
@@ -6,24 +6,32 @@ var request = require('request');
 var secret = require('./secret');
 
 var INPUT_FILE_PATH = './data_test.csv';
-var OUTPUT_FILE_NAME = 'out.txt';
+var OUTPUT_FILE_NAME = 'out.json';
 
 var main = (cb) => {
 	var outputStream = fs.createWriteStream(OUTPUT_FILE_NAME);
-	var parser = parse({delimiter: ','}, function (err, data) {
-	  async.eachSeries(data, (line, sCb) => {
-	    if (line[0] == 'Voter File VANID') {
-	    	// skip first line of column headers
-	    	return setImmediate(sCb);	
-	    }
-	    processLine(outputStream, line, sCb);
-	  }, (err) => {
-			if (err) return cb(err);
-	  	outputStream.end();
-	  	return cb();
-	  });
+	var lineCounter = 0;
+	outputStream.write('', (err) => {
+		if (err) return cb(err);
+		var parser = parse({delimiter: ','}, function (err, data) {
+		  async.eachSeries(data, (line, sCb) => {
+		  	console.log(`line ${lineCounter++}`);
+		    if (line[0] == 'Voter File VANID') {
+		    	// skip first line of column headers
+		    	return setImmediate(sCb);	
+		    }
+		    processLine(outputStream, line, sCb);
+		  }, (err) => {
+				if (err) return cb(err);
+		  	outputStream.write(']', (err) => {
+		  		if (err) return cb(err);
+		  		outputStream.end();
+		  		return cb();
+		  	});
+		  });
+		});
+		fs.createReadStream(INPUT_FILE_PATH).pipe(parser);
 	});
-	fs.createReadStream(INPUT_FILE_PATH).pipe(parser);
 }
 
 
@@ -34,11 +42,79 @@ var processLine = (stream, line, cb) => {
 	var homePhone = line[3];
 	var workPhone = line[4];
 	var workExt = line[5];
-	twilioLookup(preferredPhone, (err) => {
-		if (err) return cb(err);
-		stream.write(`${JSON.stringify({vanid, preferredPhone})}\n`, cb);	
+
+	var errorCodes = {
+		preferredPhone: null,
+		cellPhone: null,
+		homePhone: null,
+		workPhone: null
+	};
+
+	async.series([
+		(sCb) => {
+			// lookup preferred phone
+			if (preferredPhone) {
+				twilioLookup(preferredPhone, (err, body) => {
+					if (err) return sCb(err);
+					errorCodes.preferredPhone = body.carrier.error_code;
+					return sCb()
+				});
+			} else {
+				return sCb()
+			}
+		},
+		(sCb) => {
+			// lookup cell phone
+			if (cellPhone) {
+				twilioLookup(cellPhone, (err, body) => {
+					if (err) return sCb(err);
+					errorCodes.cellPhone = body.carrier.error_code;
+					return sCb();
+				});
+			} else {
+				return sCb();
+			}
+		},
+		(sCb) => {
+			// lookup home phone
+			if (homePhone) {
+				twilioLookup(homePhone, (err, body) => {
+					if (err) return sCb(err);
+					errorCodes.homePhone = body.carrier.error_code;
+					return sCb();
+				});
+			} else {
+				return sCb();
+			}
+		},
+		(sCb) => {
+			// lookup work phone
+			if (workPhone) {
+				twilioLookup(workPhone, (err, body) => {
+					if (err) return sCb(err);
+					errorCodes.workPhone = body.carrier.error_code;
+					return sCb();
+				});
+			} else {
+				return sCb();
+			}
+		}
+	], (err) => {
+		if(err) return cb(err);
+		var outputLine = {
+			'Voter File VANID': vanid,
+			'Preferred Phone': preferredPhone,
+			'Cell Phone': cellPhone,
+			'Home Phone': homePhone,
+			'WorkPhone': workPhone,
+			'WorkPhoneExt': workExt,
+			'Preferred Phone Error': errorCodes.preferredPhone || null,
+			'Cell Phone Error': errorCodes.cellPhone || null,
+			'Home Phone Error': errorCodes.homePhone || null,
+			'Work Phone Error': errorCodes.workPhone || null
+		};
+		stream.write(`${JSON.stringify(outputLine)},\n`, cb);	
 	});
-	
 }
 
 var twilioLookup = (phoneNumber, cb) => {
@@ -48,15 +124,14 @@ var twilioLookup = (phoneNumber, cb) => {
 			user: secret.user,
 			pass: secret.pass
 		}
-	}
+	};
 	request(url(phoneNumber), options, (err, res, body) => {
 		if (err) return cb(err);
 		if (res.statusCode != 200) {
 			return cb(res);
 		}
-		console.log({body});
-		return cb();
-	})
+		return cb(null, JSON.parse(body));
+	});
 }
 
 
